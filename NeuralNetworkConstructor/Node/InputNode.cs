7ddca44beb1b2b0b993d60eb6ce06682e64ad714
A1essandro <alexander.yermakov.1990@gmail.com>
2017-07-05 23:24:05 +0400

    Add InputNode

---
+++ NeuralNetworkConstructor/Node/InputNode.cs
@@ -0,0 +1,19 @@
+﻿namespace NeuralNetworkConstructor.Node
+{
+    public class InputNode : IInput<double>, INode
+    {
+
+        private double _data;
+
+        public void Input(double input)
+        {
+            _data = input;
+        }
+
+        public double Output()
+        {
+            return _data;
+        }
+
+    }
+}
---
+++ Tests/NodeTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NeuralNetworkConstructor.Node;
+
+namespace Tests
+{
+    [TestClass]
+    public class NodeTest
+    {
+
+        private static Random Random => new Random();
+
+        [TestMethod]
+        public void TestInputNode()
+        {
+            var node = new InputNode();
+            var value = Random.NextDouble();
+            node.Input(value);
+
+            Assert.AreEqual(value, node.Output());
+        }
+
+        [TestMethod]
+        public void TestBias()
+        {
+            Assert.AreEqual(1, new Bias().Output());
+        }
+    }
+}
