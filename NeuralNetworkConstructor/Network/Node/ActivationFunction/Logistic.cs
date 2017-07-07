2e37f21c04b4a09362c3bf44bed5461c7b0fa516
A1essandro <alexander.yermakov.1990@gmail.com>
2017-07-08 00:18:00 +0400

    Add Logistic activation function

---
+++ NeuralNetworkConstructor/Network/Node/ActivationFunction/Logistic.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace NeuralNetworkConstructor.Network.Node.ActivationFunction
+{
+    public  class Logistic : IActivationFunction
+    {
+
+        private double _param;
+
+        public Logistic(double param = 1)
+        {
+            _param = param;
+        }
+
+        public double Calculate(double value) => 1 / (1 + Math.Exp(-_param * value));
+    }
+}
