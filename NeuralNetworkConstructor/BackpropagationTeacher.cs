389dcbc3f43fbf06d50530978c90c445d12dbf67
A1essandro <alexander.yermakov.1990@gmail.com>
2017-07-07 19:53:16 +0400

    Add BackpropagationTeacher

---
+++ NeuralNetworkConstructor/BackpropagationTeacher.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NeuralNetworkConstructor.Network;
+using NeuralNetworkConstructor.Network.Node;
+
+namespace NeuralNetworkConstructor
+{
+    public class BackpropagationTeacher
+    {
+        private readonly INetwork _network;
+        private readonly double _theta;
+
+        public BackpropagationTeacher(INetwork network, double theta)
+        {
+            _network = network;
+            _theta = theta;
+        }
+
+        public void Teach(ICollection<double> input, ICollection<double> expectation)
+        {
+            _network.Input(input);
+            var output = _network.Output().ToArray();
+            var outputLayer = true;
+            var sigmas = new List<NeuronSigma>();
+
+            foreach (var layer in _network.Layers.Reverse())
+            {
+                var oIndex = 0;
+                foreach (var node in layer.Nodes.Where(n => n is Neuron))
+                {
+                    var neuron = (Neuron)node;
+                    var neuronOutput = neuron.Output();
+
+                    var sigma = outputLayer
+                        ? SigmaCalcForOutputLayer(expectation.ToArray(), neuronOutput, output, oIndex)
+                        : SigmaCalcForInnerLayers(neuronOutput, sigmas, neuron);
+
+                    sigmas.Add(new NeuronSigma(neuron, sigma));
+                    ChangeWeights(neuron, sigma);
+                    oIndex++;
+                }
+                outputLayer = false;
+            }
+        }
+
+        private static double SigmaCalcForInnerLayers(double neuronOutput, IEnumerable<NeuronSigma> sigmas, INode neuron)
+        {
+            return neuronOutput * (1 - neuronOutput) * GetChildSigmas(sigmas, neuron);
+        }
+
+        private static double SigmaCalcForOutputLayer(IReadOnlyList<double> expectation, double neuronOutput, IReadOnlyList<double> output, int oIndex)
+        {
+            return neuronOutput * (1 - neuronOutput) * (expectation[oIndex] - output[oIndex]);
+        }
+
+        private void ChangeWeights(Neuron neuron, double sigma)
+        {
+            foreach (var synapse in neuron.Synapses)
+            {
+                synapse.ChangeWeight(_theta * sigma * synapse.MasterNode.Output());
+            }
+        }
+
+        private static double GetChildSigmas(IEnumerable<NeuronSigma> sigmas, INode neuron)
+        {
+            double sigma = 0;
+
+            //TODO: Need refactoring:
+            foreach (var neuronSigma in sigmas)
+            {
+                foreach (var synapse in neuronSigma.Neuron.Synapses.Where(s => s.MasterNode == neuron))
+                {
+                    sigma += synapse.Weight * neuronSigma.Sigma;
+                }
+            }
+            return sigma;
+        }
+
+        private class NeuronSigma
+        {
+
+            public NeuronSigma(Neuron neuron, double sigma)
+            {
+                Neuron = neuron;
+                Sigma = sigma;
+            }
+
+            public Neuron Neuron { get; }
+            public double Sigma { get; }
+        }
+
+    }
+}
--- NeuralNetworkConstructor/Network/INetwork.cs
+++ NeuralNetworkConstructor/Network/INetwork.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections.Generic;
 using NeuralNetworkConstructor.Common;
+using NeuralNetworkConstructor.Network.Layer;
 
 namespace NeuralNetworkConstructor.Network
 {
     public interface INetwork : IOutput<IEnumerable<double>>, IInput<ICollection<double>>
     {
+        ICollection<ILayer> Layers { get; }
     }
 }
