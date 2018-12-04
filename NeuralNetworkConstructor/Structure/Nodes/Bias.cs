008d07d253634874842ff5b1b42e26cdd9c555ca
A1essandro <alexander.yermakov.1990@gmail.com>
2018-12-04 19:01:15 +0400

    Invoke OnOutput in Bias.Output()

--- NeuralNetworkConstructor/Structure/Nodes/Bias.cs
+++ NeuralNetworkConstructor/Structure/Nodes/Bias.cs
@@ -3,12 +3,18 @@ using System.Threading.Tasks;
 
 namespace NeuralNetworkConstructor.Structure.Nodes
 {
-    public class Bias : IMasterNode, INotInputNode
+    public sealed class Bias : IMasterNode, INotInputNode
     {
-        public event Action<double> OnOutput;
-        private const double VALUE = 1.0; 
 
-        public Task<double> Output() => Task.FromResult(VALUE);
+        public event Action<double> OnOutput;
+
+        private const double VALUE = 1.0;
+
+        public Task<double> Output()
+        {
+            OnOutput?.Invoke(VALUE);
+            return Task.FromResult(VALUE);
+        }
 
     }
 }
